3a8f50022706b4c71c813a5fa5df21a24ef9be88
Cam <cpbuckingham@users.noreply.github.com>
2022-02-03 13:53:05 -0600

    Update demo.js

--- examples/demo.js
+++ examples/demo.js
@@ -1,6 +1,17 @@
 /* eslint-disable no-console */
 const animalHash = require('../lib')
 
-Array.from(Array(11), () => {
-  console.log(animalHash(Math.random()))
-})
+// Array.from(Array(11), () => {
+//   console.log(animalHash(Math.random()))
+// })
+
+   console.log("Tommy S. - " + animalHash(Math.random()))
+   console.log("Tori S. - " + animalHash(Math.random()))
+   console.log("Alex N. - " + animalHash(Math.random()))
+   console.log("Eddie Y. - " + animalHash(Math.random()))
+   console.log("Prerak P. - " + animalHash(Math.random()))
+   console.log("Martin F. - " + animalHash(Math.random()))
+   console.log("Amar A. - " + animalHash(Math.random()))
+   console.log("Jacob W. - " + animalHash(Math.random()))
+   console.log("Cam B. - " + animalHash(Math.random()))
+
